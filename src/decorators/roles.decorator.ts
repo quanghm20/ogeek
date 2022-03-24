6e4852cbfb5a5d29f4bbe86cf26f70d4ead013a9
Thái Lê Sỹ <thai.ls@geekup.vn>
2022-03-24 16:17:36 +0700

    fix: package json

--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -4,4 +4,4 @@ import { SetMetadata } from '@nestjs/common';
 
 import { RoleType } from '../common/constants/role-type';
 
-export const Roles = (...roles: RoleType[]) => SetMetadata('role', roles);
+export const Roles = (...roles: RoleType[]) => SetMetadata('roles', roles);
